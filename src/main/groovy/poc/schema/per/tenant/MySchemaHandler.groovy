704b4277e2c246f8d92525ecd72127e2d7d1f40d
Marcin Szlagor <marcin.szlagor@adverity.com>
2023-08-11 22:57:31 +0200

    Adding comments

--- src/main/groovy/poc/schema/per/tenant/MySchemaHandler.groovy
+++ src/main/groovy/poc/schema/per/tenant/MySchemaHandler.groovy
@@ -21,7 +21,11 @@ class MySchemaHandler extends DefaultSchemaHandler {
     }
 
     MySchemaHandler() {
-//        super("SET SCHEMA '%s'", "CREATE SCHEMA '%s'", "public")
+        // TODO
+        // super("SET SCHEMA '%s'", "CREATE SCHEMA '%s'", "public") => is not working fine on postgres.
+        // CREATE IF NOT EXISTS is a workaround for postgres issue with 'SET SCHEMA' not failing when given schema does not exists.
+        // HibernateDataStore.addTenantForSchemaInternal relies on useSchema to fail, so it can be created in that case.
+        // As this workaround is a simplification, in production code we should rather manually check if the schema exists and act accordingly.
         super("CREATE SCHEMA IF NOT EXISTS %s; SET SEARCH_PATH = %s;", "CREATE SCHEMA %s", "public") // please notice lack of quotation mark in CREATE SCHEMA part
     }
 
@@ -29,7 +33,7 @@ class MySchemaHandler extends DefaultSchemaHandler {
     Collection<String> resolveSchemaNames(DataSource dataSource) {
         log.info("resolveSchemaNames()")
 
-        List existingSchemas = SchemaUtils.getExistingSchemas(dataSource)
+        List existingSchemas = SchemaUtils.getExistingSchemas(dataSource)   //TODO not really needed
 
         log.info "existingSchemas = ${existingSchemas}"
 
