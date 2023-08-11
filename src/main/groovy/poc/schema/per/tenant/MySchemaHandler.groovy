068eb63e6d94cd1344bcfb2676e2e4f30237919b
Marcin Szlagor <marcin.szlagor@adverity.com>
2023-08-11 21:46:02 +0200

    Postgres schema per tenant working - enjoy

--- src/main/groovy/poc/schema/per/tenant/MySchemaHandler.groovy
+++ src/main/groovy/poc/schema/per/tenant/MySchemaHandler.groovy
@@ -4,14 +4,25 @@ import groovy.util.logging.Slf4j
 import org.grails.datastore.gorm.jdbc.schema.DefaultSchemaHandler
 
 import javax.sql.DataSource
+import java.sql.Connection
 
 @Slf4j
 class MySchemaHandler extends DefaultSchemaHandler {
 
     private List<String> validTenants = ['amazon', 'empik']
 
+    @Override
+    void useSchema(Connection connection, String name) {
+        String useStatement = String.format(useSchemaStatement, name, name)
+        log.debug("Executing SQL Set Schema Statement: ${useStatement}")
+        connection
+                .createStatement()
+                .execute(useStatement)
+    }
+
     MySchemaHandler() {
-        super("SET SCHEMA '%s'", "CREATE SCHEMA '%s'", "public")
+//        super("SET SCHEMA '%s'", "CREATE SCHEMA '%s'", "public")
+        super("CREATE SCHEMA IF NOT EXISTS %s; SET SEARCH_PATH = %s;", "CREATE SCHEMA %s", "public") // please notice lack of quotation mark in CREATE SCHEMA part
     }
 
     @Override
